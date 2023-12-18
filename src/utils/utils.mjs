6096dbeb8e6e76878bda6a1261e6c99f1418aa8f
numselli <58607232+numselli@users.noreply.github.com>
2023-12-17 19:37:21 -0800

    Update utils.mjs

--- src/utils/utils.mjs
+++ src/utils/utils.mjs
@@ -13,7 +13,7 @@ export const postChannelYtVid = async(row, client) => {
     const vidID = lastRSS.id.replace("yt:video:", "")
 
     if (vidID === row.lastvid) return;
-    client.db.prepare('UPDATE channels SET lastvid = @lastvid, channelname = @channelname WHERE channelid = @channelid)').run({
+    client.db.prepare('UPDATE channels SET lastvid = @lastvid, channelname = @channelname WHERE channelid = @channelid').run({
         channelid: row.channelid,
         lastvid: vidID,
         channelname: lastRSS.author
