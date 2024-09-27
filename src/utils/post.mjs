e6275e3a315b8b762b4dc5bc45f17932e2dae8d2
numselli <58607232+numselli@users.noreply.github.com>
2024-09-27 12:43:51 -0700

    get ad length using sponsorBlock

--- src/utils/post.mjs
+++ src/utils/post.mjs
@@ -1,10 +1,13 @@
+import { getTotalAdLength } from './sponsorBlock.mjs'
 import { genYtUrl } from './genYtUrl.mjs'
 
 export const postToDiscord = async(disocrdChannel, client, lastRSS)=>{
+    const totalAdLength = await getTotalAdLength(lastRSS.videoID)
+
     const videoUrl = genYtUrl(lastRSS.videoID)
 
-    client.rest.channels.createMessage(disocrdChannel, {
-        content: `# ${lastRSS.author} posted a new video\n${videoUrl}`,
+    const message = await client.rest.channels.createMessage(disocrdChannel, {
+        content: `# ${lastRSS.author} posted a new video\nAD length:${new Date(totalAdLength*1000).toISOString().slice(11, 19)}\n${videoUrl}`,
         components: [
             {
                 type: 1,
---
+++ src/utils/sponsorBlock.mjs
@@ -0,0 +1,13 @@
+export const getTotalAdLength = async(videoID) => {
+    const rawFetch = await fetch(`https://sponsor.ajay.app/api/skipSegments?videoID=${videoID}`)
+
+    if (rawFetch.status === 404) return 0
+
+    const skipSegments = await rawFetch.json()
+
+    const totalAdLength = skipSegments.map(skipSegment => 
+        skipSegment.segment.reverse().reduce((accumulator, item) =>  accumulator - item))
+        .reduce((total, num) => total + num, 0)
+
+    return totalAdLength
+}
