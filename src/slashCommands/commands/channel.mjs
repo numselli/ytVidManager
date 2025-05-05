f305423ae5bb1ebe07fbb07f3ca7bc708a26bae9
numselli <58607232+numselli@users.noreply.github.com>
2025-05-04 17:47:52 -0700

    Update channel.mjs

--- src/slashCommands/commands/channel.mjs
+++ src/slashCommands/commands/channel.mjs
@@ -90,7 +90,7 @@ export default {
                     }
                 }).catch(() => {});
 
-                parts.forEach((element, index) => {
+                messageParts.forEach((element, index) => {
                     if (index === 0) return;
                     client.rest.channels.createMessage(interaction.channelID, {
                         content: element
