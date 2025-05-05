363e3ee0c81db78747808226dcc7aaa6fd260087
numselli <58607232+numselli@users.noreply.github.com>
2025-05-04 19:35:15 -0700

    Delete rss.mjs

--- src/utils/rss.mjs
+++
@@ -1,7 +0,0 @@
-import rssParser from "./rssParser.mjs";
-
-export default async(channelid) => {
-	const feed = await rssParser(`https://www.youtube.com/feeds/videos.xml?channel_id=${channelid}`);
-
-    return feed.items[0]
-}
