7430c33c7a5c762bb65b5d9ea1488cf50c3f01db
numselli <58607232+numselli@users.noreply.github.com>
2025-11-12 16:43:55 -0800

    Update commandList.mjs

--- src/slashCommands/commandList.mjs
+++ src/slashCommands/commandList.mjs
@@ -1,9 +1,11 @@
 import channel from './commands/channel.mjs'
 import videos from './commands/videos.mjs';
+import post from './commands/videos.mjs';
 
 export const commandList = [
 	channel,
-	videos
+	videos,
+	post
 ];
 
 export const clientCommands = new Map(
