6eff9047e183343cee1a261ab564a95e8d0124f2
numselli <58607232+numselli@users.noreply.github.com>
2026-06-12 07:22:40 -0700

    Update rssParser.mjs

--- src/utils/rssParser.mjs
+++ src/utils/rssParser.mjs
@@ -2,14 +2,36 @@
 import XMLParser from "./fast-xml-parser/xmlparser/XMLParser";
 
 const parser = new XMLParser();
+let redditBlockedUntil = 0;
+const isRedditFeed = (url) => url.includes('reddit.com')
 
-export default async(feedUrl) => {
+export default async (feedUrl) => {
+  const now = Date.now();
+
+  if (isRedditFeed(feedUrl) && now < redditBlockedUntil) return {
+    error: true,
+    code: 429,
+    retryAfter: (redditBlockedUntil - now),
+    rateLimited: true
+  };
+
   const req = await fetch(feedUrl, {
     headers: {
       'User-Agent': 'rss-parser',
       'Accept': 'application/rss+xml',
     }
   })
+
+  if (isRedditFeed(feedUrl) && req.status === 429) {
+    const retryAfterHeader = req.headers.get('x-ratelimit-reset');
+    redditBlockedUntil = now + Number(retryAfterHeader);
+    return {
+      error: true,
+      code: 429,
+      retryAfter: retryAfterHeader,
+      rateLimited: true
+    }
+  }
 
   if (req.status !== 200) return {error: true, code: req.status}
 
@@ -22,12 +44,12 @@ export default async(feedUrl) => {
       title: feed.title,
       items: feed.entry ? (Array.isArray(feed.entry) ? feed.entry.map(formatYTEntry) : [formatYTEntry(feed.entry)]).filter(a=>a) : []
     }
-  } else {
-    return {
-      updated: feed.updated,
-      title: feed.title,
-      entry: (Array.isArray(feed.entry) ? feed.entry.map(formatRdtEntry) : [formatRdtEntry(feed.entry)]).filter(a=>a)
-    }
+  }
+
+  return {
+    updated: feed.updated,
+    title: feed.title,
+    entry: (Array.isArray(feed.entry) ? feed.entry.map(formatRdtEntry) : [formatRdtEntry(feed.entry)]).filter(a=>a)
   }
 }
 
